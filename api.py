3043a98845a0086a0a4d2d0f91066ede8d00c716
Priyanshu Trivedi <84516353+PriyanshuTrivedi@users.noreply.github.com>
2024-04-28 23:24:59 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 from flask import Flask,request,jsonify
+from flask_cors import CORS, cross_origin
 from detect_heart import *
 import numpy as np
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/',methods=["GET"])
 def home():
